6a45b2612606f3468a19cc489002bc2f6e09054c
Long, William <William.Long@fmr.com>
2018-06-12 15:55:12 -0400

    working on food tracker app tutorial

---
+++ ios learning/ios learning/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  ios learning
+//
+//  Created by Long, William on 6/12/18.
+//  Copyright © 2018 Long, William. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // MARK: initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: Private Methods
+    private func setUpButtons() {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+    }
+}
