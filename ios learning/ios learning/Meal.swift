0f0277960899410971fe419f20fc13351509552c
Long, William <William.Long@fmr.com>
2018-06-19 11:03:26 -0400

    added data model

---
+++ ios learning/ios learning/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  ios learning
+//
+//  Created by Long, William on 6/19/18.
+//  Copyright © 2018 Long, William. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialization should fail if there is no name or if the rating is negative.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
